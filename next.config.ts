665541b85e5a1a30dc4ef0a77ed7c469d32b0b57
karimbobboi <abdulkarimbobboi@gmail.com>
2025-10-02 22:44:20 +0100

    refactor next.config.ts to remove production-specific configurations

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  basePath: isProd ? "/repo-name" : "",
-  assetPrefix: isProd ? "/repo-name/" : "",
-  images: {
-    unoptimized: true, // required for static export if you use next/image
-  },
-};
-
+  reactStrictMode: true,
+  images: { unoptimized: true }, // optional if you want local image handling
+}
 export default nextConfig;
