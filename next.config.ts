1d6c0e7fb64aaf08edd425174293a59bb9acb83e
karimbobboi <abdulkarimbobboi@gmail.com>
2025-10-02 22:41:46 +0100

    remove output configuration for static export in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  output: "export", // ensures static export (Next.js 13+)
   basePath: isProd ? "/repo-name" : "",
   assetPrefix: isProd ? "/repo-name/" : "",
   images: {
